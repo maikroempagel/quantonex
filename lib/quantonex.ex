ca4169960ba6ad8450803d1e55b2c76b609cd108
Maik Roempagel <maik.roempagel@gmail.com>
2019-12-03 13:52:48 +0100

    Add initial project

---
+++ lib/quantonex.ex
@@ -0,0 +1,18 @@
+defmodule Quantonex do
+  @moduledoc """
+  Documentation for Quantonex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quantonex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quantonex_test.exs
@@ -0,0 +1,8 @@
+defmodule QuantonexTest do
+  use ExUnit.Case
+  doctest Quantonex
+
+  test "greets the world" do
+    assert Quantonex.hello() == :world
+  end
+end
