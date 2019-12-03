96a2a850e74eb922e275fc9903882970fef9330b
Maik Roempagel <maik.roempagel@gmail.com>
2019-12-03 20:10:42 +0100

    Add SMA implementation

--- lib/quantonex.ex
+++ lib/quantonex.ex
@@ -3,16 +3,33 @@ defmodule Quantonex do
   Documentation for Quantonex.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Quantonex.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def sma(data) do
+    data |> sma(length(data))
   end
+
+  def sma(data, _period) when length(data) < 2,
+    do: {:error, "There must be at least 2 data points."}
+
+  def sma(_data, period) when period < 2, do: {:error, "Period must be at least 2."}
+
+  def sma(data, period) do
+    try do
+      value =
+        data
+        |> Enum.reverse()
+        |> Enum.take(period)
+        |> Enum.map(&create_decimal/1)
+        |> Enum.reduce(fn x, acc -> Decimal.add(x, acc) end)
+        |> Decimal.div(period)
+
+      {:ok, value}
+    rescue
+      _e in Decimal.Error ->
+        {:error, "One of the data points is non-numeric."}
+    end
+  end
+
+  defp create_decimal(value) when is_float(value), do: Decimal.from_float(value)
+
+  defp create_decimal(value), do: Decimal.new(value)
 end
--- test/quantonex_test.exs
+++ test/quantonex_test.exs
@@ -1,8 +1,39 @@
 defmodule QuantonexTest do
   use ExUnit.Case
-  doctest Quantonex
 
-  test "greets the world" do
-    assert Quantonex.hello() == :world
+  @dataset_error "There must be at least 2 data points."
+  @period_error "Period must be at least 2."
+
+  test "invalid empty data set and implicit period" do
+    assert Quantonex.sma([]) == {:error, @dataset_error}
+  end
+
+  test "invalid empty data set and explicit invalid period" do
+    assert Quantonex.sma([], 1) == {:error, @dataset_error}
+  end
+
+  test "invalid empty data set and explicit valid period" do
+    assert Quantonex.sma([], 2) == {:error, @dataset_error}
+  end
+
+  test "invalid non-empty data set and implicit period" do
+    assert Quantonex.sma([1]) == {:error, @dataset_error}
+  end
+
+  test "valid non-empty data set and explicit invalid period" do
+    assert Quantonex.sma([1, 2, 3], 1) == {:error, @period_error}
+  end
+
+  test "valid non-empty integer data set and implicit period" do
+    assert Quantonex.sma([1, 2, 3]) == {:ok, Decimal.new(2)}
+  end
+
+  test "valid non-empty float data set and implicit period" do
+    assert Quantonex.sma([2.1, 2.0, -2.0]) == {:ok, Decimal.from_float(0.7)}
+  end
+
+  test "invalid non-numeric data set" do
+    data = ["a", "b"]
+    assert Quantonex.sma(data) == {:error, "One of the data points is non-numeric."}
   end
 end
