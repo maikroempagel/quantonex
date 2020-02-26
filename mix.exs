3824931f81208ae8719d047737c331dad515cd9b
Maik Roempagel <maik.roempagel@gmail.com>
2020-02-26 20:27:53 +0100

    Add Credo and ExCoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Quantonex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +28,10 @@ defmodule Quantonex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.2"},
       {:decimal, "~> 1.8"},
-      {:ex_doc, "~> 0.21.3"}
+      {:ex_doc, "~> 0.21.3"},
+      {:excoveralls, "~> 0.12.2"}
     ]
   end
 end
