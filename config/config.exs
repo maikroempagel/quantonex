2e88ed999f3df74d7eceac4d02846e9fd2db9ff9
Maik Roempagel <maik.roempagel@gmail.com>
2020-02-29 23:06:45 +0100

    build: Add git_ops configuration

---
+++ config/config.exs
@@ -0,0 +1,25 @@
+import Config
+
+config :git_ops,
+  mix_project: Quantonex.MixProject,
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/maikroempagel/quantonex",
+  types: [
+    # Makes an allowed commit type called `tidbit` that is not
+    # shown in the changelog
+    tidbit: [
+      hidden?: true
+    ],
+    # Makes an allowed commit type called `important` that gets
+    # a section in the changelog with the header "Important Changes"
+    important: [
+      header: "Important Changes"
+    ]
+  ],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md",
+  version_tag_prefix: "v"
